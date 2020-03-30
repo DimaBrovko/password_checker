44646747b61702a6e729e07a8f2cfd185121b2d7
DimaBrovko <halixfax@gmail.com>
2020-03-30 14:38:09 +0300

    argv password checker(exit)

--- checkpass.py
+++ checkpass.py
@@ -31,5 +31,5 @@ def main(args):
         else:
             print(f'{password} was not found.Good password.')
     return 'done'
-
-main(sys.argv[1:])
+if __name__=='__main__':
+    sys.exit(main(sys.argv[1:]))
